826c0ca5605e0bdd83e0ebf5bdcb503b3053084a
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-10-13 10:26:27 +0600

    dropdown added

---
+++ src/components/UI/NavbarDropdown.tsx
@@ -0,0 +1,54 @@
+"use client";
+import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+
+export default function NavbarDropdown() {
+  const router = useRouter();
+  const handleNavigation = (pathName: string) => {
+    router.push(pathName);
+  };
+  return (
+    <div>
+      <Dropdown>
+        <DropdownTrigger>
+          <Avatar className="cursor-pointer" name="Joe" />
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Static Actions">
+          <DropdownItem
+            onClick={() => handleNavigation("/profile")}
+            key="profile"
+          >
+            Profile
+          </DropdownItem>
+          <DropdownItem
+            onClick={() => handleNavigation("/create-post")}
+            key="post"
+          >
+            Create Post
+          </DropdownItem>
+          <DropdownItem
+            onClick={() => handleNavigation("/claim-request")}
+            key="request"
+          >
+            Claim Request
+          </DropdownItem>
+          <DropdownItem
+            onClick={() => handleNavigation("/setting")}
+            key="setting"
+          >
+            Setting
+          </DropdownItem>
+          <DropdownItem key="delete" className="text-danger" color="danger">
+            Logout
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  );
+}
--- src/components/UI/navbar.tsx
+++ src/components/UI/navbar.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/order */
 "use client";
 import {
   Navbar as NextUINavbar,
@@ -8,40 +10,17 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
 
 import { siteConfig } from "@/src/config/site";
 import { ThemeSwitch } from "@/src/components/UI/theme-switch";
-import { SearchIcon, Logo } from "@/src/components/icons";
+import { Logo } from "@/src/components/icons";
+import NavbarDropdown from "./NavbarDropdown";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -75,6 +54,9 @@ export const Navbar = () => {
       >
         <NavbarItem className="hidden sm:flex gap-2">
           <ThemeSwitch />
+        </NavbarItem>
+        <NavbarItem className="hidden sm:flex gap-2">
+          <NavbarDropdown />
         </NavbarItem>
       </NavbarContent>
 
