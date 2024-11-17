609ee3a31914085561d8486a2e549b49ee077564
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-11-17 11:12:21 +0600

    image gallery created

--- src/app/(commonLayout)/found-item/page.tsx
+++ src/app/(commonLayout)/found-item/page.tsx
@@ -1,3 +1,17 @@
-export default function FoundItem() {
-  return <div>FoundItem</div>;
+import Container from "@/src/components/UI/Container";
+import Post from "@/src/components/UI/Post";
+import axiosInstance from "@/src/config/axios.config";
+import { IPost } from "@/src/types";
+
+export default async function FoundItem() {
+  const { data } = await axiosInstance.get(`/items`);
+  return (
+    <Container>
+      <div className="mx-auto my-3 max-w-[720px]">
+        {data?.data?.map((post: IPost) => (
+          <Post key={post?._id} post={post} />
+        ))}
+      </div>
+    </Container>
+  );
 }
